46477b4553382634678346df13928b8599ef8072
garikinasailu <garikinasailu6@gmail.com>
2025-02-23 07:29:47 +0530

    Add files via upload

---
+++ conditional_task.py
@@ -0,0 +1,71 @@
+# write a python program that take a characters as input and checks whether  it is vowel or not .use the if else statement
+
+
+#vowel=input("enter the vowel:")
+#character=["a","e","i","o","u"]
+#if vowel in character:
+#    print("it is vowel")
+#else:
+#    print("it is not a vowel")
+
+
+#
+#age=int(input("enter the age:"))
+#if age<=12:
+#     print(f"your {age} years older, your child")
+#elif age<=17:
+#    print(f"your{age} years older,your teenager")
+#elif age<=64:
+#    print (f"your{age} years older,your adult")
+#else:
+#   print("your 65 older,your senior")
+
+
+#integer=int(input("enter the  number:"))
+#if 0<integer:
+#    print("it is postive number")
+#elif 0>integer:
+#    print("it is negative number")
+#else:
+#    print("it is zero")
+
+#num_1=int(input("enter the number_1:"))
+#num_2=int(input("enter the number_2:"))
+#operator=(input("enter the operations:"))
+#if operator =="+":
+#    print(num_1+num_2)
+#elif operator=="-":
+#    print(num_1-num_2) 
+#elif operator=="*":
+#    print(num_1*num_2)
+#elif operator=="/":
+#    print(num_1/num_2)
+#else:
+#    print("invaild the numbers")
+
+#num=int(input("enter the number:"))
+#print(f"{num} is even") if num%2==0 else print(f"{num} is odd ")
+
+#price=int(input("enter the price:"))
+#discount=int(input("enter the discount:"))
+#result=price*(discount/100 )
+#print(result)
+
+weight=int(input("enter the weight:"))
+height=float(input("enter the height:"))
+bmi=weight/height**2
+print(bmi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
